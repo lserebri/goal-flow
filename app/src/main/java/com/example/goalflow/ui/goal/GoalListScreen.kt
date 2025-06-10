a0f1436e225d910e9fbbf7266d234e7f3139558e
Leo Serebriakov <lserebri@Leos-MacBook-Air.local>
2025-06-10 16:53:28 +0100

    new design solution

--- app/src/main/java/com/example/goalflow/ui/goal/GoalListScreen.kt
+++ app/src/main/java/com/example/goalflow/ui/goal/GoalListScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -94,9 +95,13 @@ fun GoalListScreen(goalViewModel: GoalViewModel = hiltViewModel()) {
             }
         }
 
+//        Spacer(modifier = Modifier.size(50.dp))
+
+
         Button(
             onClick = { showDialog = true },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
             Text("Add Goal")
         }
--- app/src/main/java/com/example/goalflow/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/goalflow/ui/home/HomeScreen.kt
@@ -1,8 +1,16 @@
 package com.example.goalflow.ui.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,24 +18,105 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.*
+import androidx.navigation.compose.rememberNavController
+import com.example.goalflow.ui.consumable.ConsumableListScreen
+import com.example.goalflow.ui.goal.GoalListScreen
+
+data class Destination(
+    val name: String,
+    val route: String
+)
+
+val destinations = listOf(
+    Destination("Goals", "goals"),
+    Destination("Consumables", "consumables")
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationTab(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val startDestination = Destination("Goals", "goals")
+    var selectedDestination by rememberSaveable { mutableIntStateOf(destinations.indexOf(startDestination)) }
+
+    Column(modifier = modifier) {
+        SecondaryTabRow(selectedTabIndex = selectedDestination) {
+            destinations.forEachIndexed { index, destination ->
+                Tab(
+                    selected = selectedDestination == index,
+                    onClick = {
+                        navController.navigate(destination.route) {
+                            // Avoid building up a back stack of tabs
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                        selectedDestination = index
+                    },
+                    text = {
+                        Text(
+                            text = destination.name,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                )
+            }
+        }
+
+        NavHost(navController = navController, startDestination = "goals", modifier = Modifier.weight(1f)) {
+            composable("goals") { GoalListScreen() }
+            composable("consumables") { ConsumableListScreen() }
+        }
+    }
+}
 
 
-
-@Composable()
-fun HomeScreen(homeViewModel: HomeViewModel= hiltViewModel()) {
+@Preview
+@Composable
+fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) {
     val uiState by homeViewModel.score.collectAsState()
 
     if (uiState is ScoreUiState.Success) {
         val score = (uiState as ScoreUiState.Success).data
 
-        Box (modifier = Modifier
-            .fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ){
-            Text(
-                text = score.toString(),
-                style = MaterialTheme.typography.headlineLarge
-            )
+        Scaffold { contentPadding ->
+            Column(modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize()
+                .padding(25.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 20.dp)
+                        .weight(0.1f), // Top section for score
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = score.toString(),
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontSize = 60.sp
+                    )
+                }
+
+                Spacer(modifier = Modifier.size(100.dp))
+
+
+                NavigationTab(modifier = Modifier.weight(0.5f)) // Remaining space for tab navigation
+            }
         }
     }
 }
+
+
