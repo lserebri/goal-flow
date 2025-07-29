b7e079e9328766d75f17fd4b8bfec4127eec806e
Leo Serebriakov <lserebri@Leos-MacBook-Air.local>
2025-07-29 14:29:03 +0100

    dependencies updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.example.goalflow"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.goalflow"
@@ -66,13 +66,13 @@ dependencies {
     ksp("com.google.dagger:hilt-android-compiler:2.57")
 
     // Fragment
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.9.2")
 
     // Navigation
-    implementation ("androidx.navigation:navigation-ui-ktx:2.9.0")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.9.2")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // Room
     implementation(libs.room.runtime)
