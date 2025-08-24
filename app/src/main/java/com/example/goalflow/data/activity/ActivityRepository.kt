fda1d9048e7578b130f8b584d9ea6386560d955d
lserebri <leo.serebriakov@gmail.com>
2025-08-24 14:22:00 +0100

    Dao fix

--- app/src/main/java/com/example/goalflow/data/activity/ActivityRepository.kt
+++ app/src/main/java/com/example/goalflow/data/activity/ActivityRepository.kt
@@ -2,10 +2,10 @@ package com.example.goalflow.data.activity
 
 import kotlinx.coroutines.flow.Flow
 
-open class ActivityRepository<T : ActivityItem>(
+abstract class ActivityRepository<T : ActivityItem>(
 	private val dao: ActivityDao<T>
 ) {
-	open fun getAll(): Flow<List<T>> = dao.getAll()
+	abstract fun getAll(): Flow<List<T>>
 
 	open suspend fun insert(activity: T) = dao.insert(activity)
 
--- app/src/main/java/com/example/goalflow/data/goal/GoalDao.kt
+++ app/src/main/java/com/example/goalflow/data/goal/GoalDao.kt
@@ -5,16 +5,20 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.goalflow.data.activity.ActivityDao
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GoalDao: ActivityDao<Goal> {
     @Query("SELECT * FROM goals")
-    override fun getAll(): Flow<List<Goal>>
+    fun getAll(): Flow<List<Goal>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     override suspend fun insert(activity: Goal)
+    
+    @Update
+    override suspend fun update(activity: Goal)
 
     @Delete
     override suspend fun delete(activity: Goal)
