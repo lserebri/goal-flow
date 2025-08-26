ae6919a8870c484c1c5c299c624168fea98ff59e
lserebri <leo.serebriakov@gmail.com>
2025-08-26 19:28:39 +0100

    getting rid of unused imports and files

--- app/src/main/java/com/example/goalflow/data/activity/ActivityDao.kt
+++ app/src/main/java/com/example/goalflow/data/activity/ActivityDao.kt
@@ -3,7 +3,9 @@ package com.example.goalflow.data.activity
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 interface ActivityDao<T : ActivityItem> {
 	@Insert(onConflict = OnConflictStrategy.REPLACE)
